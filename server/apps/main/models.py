ff176c25b2c97f487f6545ccbad48e0d2564b911
mridubhatnagar <mridubhatnagar@rediffmail.com>
2021-08-24 18:37:56 +0530

    modified fields in model

--- server/apps/main/models.py
+++ server/apps/main/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from openhumans.models import OpenHumansMember
 
 
-class PublicExperience(models.Model):
-    experience_text = models.TextField()
-    difference_text = models.TextField(default="")
+class Experience(models.Model):
+    experience = models.TextField()
+    suggestion = models.TextField(default="")
     created_at = models.DateTimeField(auto_now=True)
     experience_id = models.TextField()
+    permission = models.TextField()
     open_humans_member = models.ForeignKey(OpenHumansMember,
                                            blank=True, null=True,
                                            on_delete=models.CASCADE)
     approved = models.CharField(
         blank=False,
-        default='not reviewed',
+        default='draft',
         max_length=50)
