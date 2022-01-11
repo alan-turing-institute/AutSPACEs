9d81342bb9e4b4e25300a7ab4b75638d66d45fdc
Soledad <soledad.lxj@gmail.com>
2022-01-11 14:43:25 +0100

    fix: log navbar

--- server/apps/main/views.py
+++ server/apps/main/views.py
@@ -222,14 +222,31 @@ def confirmation_page(request):
     """
     return render(request, "main/confirmation_page.html")
 
+
+# def about_us(request):
+#     auth_url = OpenHumansMember.get_auth_url()
+#     context = {'auth_url': auth_url,
+#                'oh_proj_page': settings.OH_PROJ_PAGE}
+#     if request.user.is_authenticated:
+#         return redirect('main:about_us')
+#     return render(request, 'main/about_us.html', context=context)
+
+# def what_autism_is(request):
+#     auth_url = OpenHumansMember.get_auth_url()
+#     context = {'auth_url': auth_url,
+#                'oh_proj_page': settings.OH_PROJ_PAGE}
+#     if request.user.is_authenticated:
+#         return redirect('main:what_autism_is')
+#     return render(request, 'main/what_autism_is.html', context=context)
+def navigation(request):
+    return render(request, "main/navigation.html")
+
 def about_us(request):
     return render(request, "main/about_us.html")
 
 def what_autism_is(request):
     return render(request, "main/what_autism_is.html")
 
-def navigation(request):
-    return render(request, "main/navigation.html")
-
 def footer(request):
     return render(request, "main/footer.html")
+
