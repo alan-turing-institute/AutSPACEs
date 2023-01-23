409332b6ed6d6935c64472b8f54fb9db7a824e07
callummole <cmole@turing.ac.uk>
2023-01-23 14:47:57 +0000

    refactor `my_stories` to tidy up duplicate code

--- server/apps/main/templatetags/custom_tags.py
+++ server/apps/main/templatetags/custom_tags.py
@@ -4,3 +4,10 @@ register = template.Library()
 @register.simple_tag
 def define(val=None):
   return val
+
+@register.simple_tag
+def toggle_story(val):
+  if val == 1:
+    return 'story'
+  else:
+    return 'stories'
