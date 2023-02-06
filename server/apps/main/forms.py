85f7f1a16c32998188b5cc20aebf2ef5b03fcf98
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2023-02-06 14:07:47 +0000

    re-word submission form

--- server/apps/main/forms.py
+++ server/apps/main/forms.py
@@ -1,27 +1,27 @@
 from django import forms
 
 class ShareExperienceForm(forms.Form):
-        
-    experience = forms.CharField(label='Please share your story', max_length=500, strip=True,
-                                 widget=forms.Textarea(attrs={'placeholder':'Write your story here',
+
+    experience = forms.CharField(label='Please share your experience', max_length=500, strip=True,
+                                 widget=forms.Textarea(attrs={'placeholder':'Write your experience here, you can take as much or little space as you need.',
                                                               'rows':'4',
                                                               'class':'form-control'}))
     experience.group = 1
-    wish_different = forms.CharField(label='Suggestions for what could have made your story better',
+    wish_different = forms.CharField(label='What could have made your experience better?',
                                      strip=True, max_length=500,
-                                     widget=forms.Textarea(attrs={'placeholder':'Write your story here',
+                                     widget=forms.Textarea(attrs={'placeholder':'Do you have anything that you would want to have changed to make it better? Or do you have any tips for others who may experience the same thing?',
                                                               'rows':'4',
                                                               'class':'form-control'}))
     wish_different.group = 1
-    title = forms.CharField(label='Choose a title for your story', 
+    title = forms.CharField(label='Choose a title for your story',
                             strip=True,
                             max_length=150,
                             widget=forms.TextInput(attrs={'placeholder':'Title',
                                                           'class':'form-control'}))
     title.group = 1
-    
-    
-    
+
+
+
     # add triggering here
     abuse = forms.BooleanField(label = 'Abuse (physical, sexual, emotional and verbal)', required=False)
     abuse.group = 2
@@ -33,13 +33,13 @@ class ShareExperienceForm(forms.Form):
     mentalhealth.group = 2
     negbody = forms.BooleanField(label = 'Negative body image', required=False)
     negbody.group = 2
-    
+
     other = forms.CharField(label='Other',
                             strip=True,
                             max_length=150,
                             widget=forms.TextInput(), required=False)
     other.group = 2
-    
+
     # sharing options
     viewable = forms.BooleanField(label = "Share on AutSPACE website", required=False)
     viewable.group = 3
@@ -55,27 +55,26 @@ class ShareExperienceForm(forms.Form):
     ]
     moderation_status = forms.ChoiceField(choices = statuses, widget = forms.Select(), required=False)
     moderation_status.group = "hidden"
-    
+
     def __init__(self, *args, **kwargs):
         """ Disable free text fields to the moderator, or disable all fields if in 'read only' mode"""
-        
+
         disable_all = kwargs.pop('disable_all', False) #disable everything
         disable_moderator = kwargs.pop('disable_moderator', False) # only the free text fields
-        
+
         super(ShareExperienceForm, self).__init__(*args, **kwargs)
 
-                
+
         for field in self.fields:
-            if field in ['experience','wish_different','title','research','viewable']: 
+            if field in ['experience','wish_different','title','research','viewable']:
                 self.fields[field].widget.attrs['disabled']= disable_moderator or disable_all
             else:
                 self.fields[field].widget.attrs['disabled']= disable_all
-                
-    
+
+
     def clean_moderation_status(self):
         mod_status = self.cleaned_data['moderation_status']
         if not mod_status:
             mod_status = 'not reviewed'
-            
+
         return mod_status
-            
