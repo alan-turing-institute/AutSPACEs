06a76a227a39a51251c9a7936ecb7a25cb4024cb
Helen Duncan <hduncan@turing.ac.uk>
2023-02-27 16:36:21 +0000

    edit update db arguments

--- server/apps/main/helpers.py
+++ server/apps/main/helpers.py
@@ -7,61 +7,81 @@ from django.forms.models import model_to_dict
 from .forms import ModerateExperienceForm
 from .models import PublicExperience, ExperienceHistory
 
+
 def is_moderator(user):
-  """return membership of moderator group"""
+    """Return membership of moderator group."""
 
-  try:
-    group = Group.objects.get(user=user)
-    return (group.name == "Moderators")
+    try:
+        group = Group.objects.get(user=user)
+        return group.name == "Moderators"
 
-  except Group.DoesNotExist:
-    return False
+    except Group.DoesNotExist:
+        return False
 
 
-def model_to_form(model, disable_moderator = False):
+def model_to_form(model, disable_moderator=False):
+    """Converts Model to form."""
     model_dict = model_to_dict(model)
 
-    form = ModerateExperienceForm({**model_dict,
-        "viewable":True #we only moderate public experiences
-    }, disable_moderator=disable_moderator)
+    form = ModerateExperienceForm(
+        {**model_dict, "viewable": True},  # we only moderate public experiences
+        disable_moderator=disable_moderator,
+    )
 
     return form
 
+
 def extract_experience_details(model):
     """
-    Extract the immutable details of the story and the sharing options only
+    Extract the immutable details of the story and the sharing options only.
     """
     model_dict = model_to_dict(model)
 
-    for key in ['abuse', 'violence', 'drug', 'mentalhealth', 'negbody', 'other', 'moderation_status']:
+    for key in [
+        "abuse",
+        "violence",
+        "drug",
+        "mentalhealth",
+        "negbody",
+        "other",
+        "moderation_status",
+    ]:
         model_dict.pop(key, None)
-    model_dict['viewable'] = True # Only moderate viewable experiences
+    model_dict["viewable"] = True  # Only moderate viewable experiences
 
     return model_dict
 
 
 def process_trigger_warnings(form):
     """
-    Get the mutable trigger warnings from the data
+    Get the mutable trigger warnings from the data.
     """
     form.is_valid()
     # Populate with values from moderation form
-    trigger_warning_dict = {k: form.cleaned_data[k] for k in form.data.keys() if k not in ['csrfmiddlewaretoken', 'research']}
+    trigger_warning_dict = {
+        k: form.cleaned_data[k]
+        for k in form.data.keys()
+        if k not in ["csrfmiddlewaretoken", "research"]
+    }
 
     # Set all others as False
-    for key in ['abuse', 'violence', 'drug', 'mentalhealth', 'negbody']:
+    for key in ["abuse", "violence", "drug", "mentalhealth", "negbody"]:
         if key not in trigger_warning_dict.keys():
             trigger_warning_dict[key] = False
-    
+
     return trigger_warning_dict
 
+
 def reformat_date_string(context):
-     """
-     Convert the date string back to a datetime object
-     """
-     for file in context['files']:
-         file['created'] = datetime.datetime.strptime(file['created'], '%Y-%m-%dT%H:%M:%S.%fZ')
-     return context
+    """
+    Convert the date string back to a datetime object.
+    """
+    for file in context["files"]:
+        file["created"] = datetime.datetime.strptime(
+            file["created"], "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+    return context
+
 
 def get_review_status(files):
     """Given a list of files, count the number of each moderation status of the publicly viewable files
@@ -73,13 +93,18 @@ def get_review_status(files):
         statuses (dict): counts of moderation statuses
     """
 
+    status_list = [
+        f["metadata"]["data"]["moderation_status"].replace(" ", "_")
+        for f in files
+        if f["metadata"]["data"]["viewable"]
+    ]
 
-    status_list = [f['metadata']['data']['moderation_status'].replace(' ','_') for f in files if f['metadata']['data']['viewable']]
-
-    statuses = {f"n_{s}":status_list.count(s) for s in set(status_list)}
+    statuses = {f"n_{s}": status_list.count(s) for s in set(status_list)}
 
     statuses["n_viewable"] = len(status_list)
-    statuses["n_moderated"] = status_list.count('approved') + status_list.count('rejected')
+    statuses["n_moderated"] = status_list.count("approved") + status_list.count(
+        "rejected"
+    )
 
     return statuses
 
@@ -98,11 +123,11 @@ def get_oh_file(ohmember, uuid):
         file (dict): dictionary representation of OpenHumans file. Returns None if uuid is not matched.
     """
     files = ohmember.list_files()
-    file = [f for f in files if f["metadata"]["uuid"]==uuid]
+    file = [f for f in files if f["metadata"]["uuid"] == uuid]
 
-    if len(file)==0:
-        file=None
-    elif len(file)>1:
+    if len(file) == 0:
+        file = None
+    elif len(file) > 1:
         raise Exception("duplicate uuids in open humans")
 
     return file[0]
@@ -118,34 +143,25 @@ def delete_single_file(uuid, ohmember):
     """
 
     ohmember.delete_single_file(file_basename=f"{uuid}.json")
-    delete_PE(uuid,ohmember)
+    delete_PE(uuid, ohmember)
 
 
 def make_tags(data):
-    """builds list of tags based on data"""
+    """Builds list of tags based on data."""
 
-    tag_map = {'viewable': {'True':'public',
-                            'False':'not public'},
-               'research': {'True':'research',
-                            'False':'non-research'},
-                'drug':    {'True': 'drugs',
-                            'False': ''},
-                'abuse':    {'True': 'abuse',
-                            'False': ''},
-                'negbody':  {'True': 'negative body',
-                            'False': ''},
-                'violence': {'True': 'violence',
-                            'False': ''},
-                'mentalhealth': {'True': 'mental health',
-                            'False': ''},
-                'moderation_status': {'True': '',
-                            'False': 'in review'}
-                            }
+    tag_map = {
+        "viewable": {"True": "public", "False": "not public"},
+        "research": {"True": "research", "False": "non-research"},
+        "drug": {"True": "drugs", "False": ""},
+        "abuse": {"True": "abuse", "False": ""},
+        "negbody": {"True": "negative body", "False": ""},
+        "violence": {"True": "violence", "False": ""},
+        "mentalhealth": {"True": "mental health", "False": ""},
+        "moderation_status": {"True": "", "False": "in review"},
+    }
 
-    tags = [tag_map[k].get(str(v))
-            for k,v in data.items()
-            if k in tag_map.keys()]
-    if data["other"] != '':
+    tags = [tag_map[k].get(str(v)) for k, v in data.items() if k in tag_map.keys()]
+    if data["other"] != "":
         tags.append("Other triggering label")
 
     return tags
@@ -160,40 +176,44 @@ def upload(data, uuid, ohmember):
         ohmember : request.user.openhumansmember
     """
 
-    output_json = {
-            'data': data,
-            'timestamp': str(datetime.datetime.now())}
+    output_json = {"data": data, "timestamp": str(datetime.datetime.now())}
 
     # by saving the output json into metadata we can access the fields easily through request.user.openhumansmember.list_files().
     metadata = {
-
-        'uuid': uuid,   
-        'description': data.get('title_text'),
-        'tags': make_tags(data),
+        "uuid": uuid,
+        "description": data.get("title_text"),
+        "tags": make_tags(data),
         **output_json,
-        }
+    }
 
-    # create stream for oh upload
+    # create stream for OH upload
     output = io.StringIO()
     output.write(json.dumps(output_json))
     output.seek(0)
 
-
     ohmember.upload(
         stream=output,
-        filename=f"{uuid}.json", #filename is Autspaces_timestamp
-        metadata=metadata)
+        filename=f"{uuid}.json",  # filename is Autspaces_timestamp
+        metadata=metadata,
+    )
 
 
 def make_uuid():
+    """Create a Universal Unique Identifier."""
     return str(uuid.uuid1())
 
+
 def delete_PE(uuid, ohmember):
-    if PublicExperience.objects.filter(experience_id=uuid, open_humans_member=ohmember).exists():
-            PublicExperience.objects.get(experience_id=uuid, open_humans_member=ohmember).delete()
+    """Delete Public Experience object with given UUID."""
+    if PublicExperience.objects.filter(
+        experience_id=uuid, open_humans_member=ohmember
+    ).exists():
+        PublicExperience.objects.get(
+            experience_id=uuid, open_humans_member=ohmember
+        ).delete()
 
 
-def update_public_experience_db(data, uuid, ohmember, change_type="Edit", change_comments=None):
+def update_public_experience_db(data, uuid, ohmember, **change_info):
     """Updates the public experience database for the given uuid.
 
     If data is tagged as viewable, an experience will be updated or inserted.
@@ -205,34 +225,42 @@ def update_public_experience_db(data, uuid, ohmember, change_type="Edit", change
         ohmember : request.user.openhumansmember
         moderation_status (str, optional): Defaults to 'in review'.
     """
+    if data.pop("viewable", False):
+        data.pop(
+            "open_humans_member", None
+        )  # Remove from dict as needs to be an object not string
+        data.pop(
+            "experience_id", None
+        )  # Remove from dict as needs to be an object not string
+        data.pop("moderation_comments", None)  # Only need when moderating
 
-    if data.pop('viewable',False):
-        data.pop('open_humans_member', None) # Remove from dict as needs to be an object not string
-        data.pop('experience_id', None) # Remove from dict as needs to be an object not string
-        data.pop('moderation_comments', None) # Only need when moderating
-        
         pe = PublicExperience(open_humans_member=ohmember, experience_id=uuid, **data)
-        
-        # .save() updates if primary key exists, inserts otherwise. 
+
+        # .save() updates if primary key exists, inserts otherwise.
         pe.save()
 
-        if pe.experiencehistory_set.count() == 0:
-            change_type = "Make Public"
-            change_comments = "Story flagged to be shared on AutSPACE website"
-        elif change_comments == None:
-            change_comments = "Story edited"
+        # If no change info has been sent then it's either new or being edited by user
+        if not change_info:
+            if pe.experiencehistory_set.count() == 0:
+                change_type = "Make Public"
+                change_comments = "Story flagged to be shared on AutSPACE website"
+            else:
+                change_type = "Edit"
+                change_comments = "Story edited"
+        else:
+            change_type = change_info.get("change_type", "Unknown Change")
+            change_comments = change_info.get("change_comments", "No Comment Made")
+
         # Produce and add the ExperienceHistory object to the public experience
-        eh = ExperienceHistory(experience=pe,
-                                change_type = change_type, 
-                                changed_at = datetime.datetime.now(),
-                                changed_by = str(ohmember),
-                                change_comments = change_comments)
+        eh = ExperienceHistory(
+            experience=pe,
+            change_type=change_type,
+            changed_at=datetime.datetime.now(),
+            changed_by=str(ohmember),
+            change_comments=change_comments,
+        )
 
         eh.save()
-        # change_type = models.TextField()
-        # changed_at = models.DateTimeField(auto_now=True)
-        # changed_by = models.TextField()
-        # change_comments = models.TextField(default="")  
-        
+
     else:
-        delete_PE(uuid,ohmember)
+        delete_PE(uuid, ohmember)
