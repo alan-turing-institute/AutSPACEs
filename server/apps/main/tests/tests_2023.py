6be3d657040520f4950a318ec0aa8360053cd4e7
Helen Duncan <hduncan@turing.ac.uk>
2023-03-13 13:36:30 +0000

    poetry use in CI

--- server/apps/main/tests/tests_2023.py
+++ server/apps/main/tests/tests_2023.py
@@ -1,11 +1,12 @@
 import vcr
-import urllib
+import urllib.request
 import json
 
 @ vcr.use_cassette('fixtures/share_experience.yaml')
 def test_share_experience():
     r = urllib.request.urlopen('https://www.openhumans.org/api/direct-sharing/project/exchange-member/').read().decode()
-    response = json.loads(r)['data'][0]['metadata']['data']
+    r_json = json.loads(r)
+    response = r_json['data'][0]['metadata']['data']
     
     assert response['title_text'] == "Story the first"
     assert "However little known the feelings or views of such a man" in response['difference_text'] 
