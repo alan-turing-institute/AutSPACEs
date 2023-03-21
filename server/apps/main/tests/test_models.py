d0a18c5605db9e9ae4a002f593db35c6bf2fecff
Helen Duncan <hduncan@mac-ati1021.home>
2023-03-21 16:22:56 +0000

    model basic test

---
+++ server/apps/main/tests/test_models.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.db import models
+from server.apps.main.models import PublicExperience
+
+from openhumans.models import OpenHumansMember
+
+
+class Models(TestCase):
+    """
+    Tests the views function
+    """
+
+    def SetUp(self):
+        """
+        Set-up for test with two users
+        """
+        # One user 
+        self.user_a = OpenHumansMember.create(oh_id=1234, access_token="abc", refresh_token="def", expires_in=1000)
+        pe_data = {"experience_text": "a",
+                   "difference_text": "b",
+                   "title_text": "c"}
+        self.public_experience = PublicExperience(open_humans_member = self.user_a, **pe_data)
+
+    def test_model_str(self):
+        pass
+
