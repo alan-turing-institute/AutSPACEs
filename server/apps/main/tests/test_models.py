ee86b874e70ed79fd0f3daff4324b203ab80b447
Helen Duncan <hduncan@mac-ati1021.home>
2023-03-21 17:03:52 +0000

    edit creation method

--- server/apps/main/tests/test_models.py
+++ server/apps/main/tests/test_models.py
@@ -14,12 +14,14 @@ class Models(TestCase):
         Set-up for test with two users
         """
         # One user 
-        self.user_a = OpenHumansMember.create(oh_id=1234, access_token="abc", refresh_token="def", expires_in=1000)
-
+        data = {"access_token": 'foo',
+        "refresh_token": 'bar',
+        "expires_in": 36000}
+        self.non_moderator_user = OpenHumansMember.create(oh_id=12345678,data=data)
         pe_data = {"experience_text": "a",
                    "difference_text": "b",
                    "title_text": "c"}
-        PublicExperience.objects.create(open_humans_member = self.user_a, **pe_data)
+        PublicExperience.objects.create(open_humans_member = self.non_moderator_user, **pe_data)
 
     def test_model_str(self):
         story_a = PublicExperience.objects.get(title_text = "c")
