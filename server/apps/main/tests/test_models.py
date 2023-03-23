20d31ac32b9475467b1e348b0bd5ef8f7bb6fd92
Helen Duncan <hduncan@mac-ati1021.home>
2023-03-23 22:10:49 +0000

    update variable name

--- server/apps/main/tests/test_models.py
+++ server/apps/main/tests/test_models.py
@@ -19,12 +19,12 @@ class ModelsTest(TestCase):
         data = {"access_token": 'foo',
         "refresh_token": 'bar',
         "expires_in": 36000}
-        self.non_moderator_user = OpenHumansMember.create(oh_id=12345678,data=data)
-        self.non_moderator_user.save()
+        self.placeholder_user = OpenHumansMember.create(oh_id=12345678,data=data)
+        self.placeholder_user.save()
         pe_data = {"experience_text": "a",
                    "difference_text": "b",
                    "title_text": "c"}
-        PublicExperience.objects.create(open_humans_member = self.non_moderator_user, **pe_data)
+        PublicExperience.objects.create(open_humans_member = self.placeholder_user, **pe_data)
 
     def test_model_str(self):
         story_a = PublicExperience.objects.get(title_text = "c")
@@ -37,5 +37,7 @@ class ModelsTest(TestCase):
         assert story_a.mentalhealth == False
         assert story_a.negbody == False
         assert story_a.violence == False
+        assert story_a.research == False
         assert story_a.other == ""
+        assert story_a.moderation_status == "not reviewed"
 
