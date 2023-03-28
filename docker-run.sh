ea1f637f9e41a927fb43655797a6956e6ce91ed8
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2023-03-28 16:27:10 +0100

    Move platform setting from DockerFile to docker-run.sh
    This change moves the enforcement of an amd64 image on M1 macs from the
    DockerFile to docker-run.sh using an environment variable. This way we
    can ensure it only applies if an M1 mac is in use.

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,9 @@
+# If this is an M1 mac, make sure we use docker QEMU amd64
+if [[ $(uname -m) == 'arm64' ]]; then
+  echo "Running on an M1 mac; setting to use amd64 images under QEMU"
+  export DOCKER_DEFAULT_PLATFORM=linux/amd64
+fi
+
 docker-compose build
 docker-compose run --rm web python manage.py migrate
 docker-compose up
