1123c3143d08c72cf4425909aab2fd652d6ded9a
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2023-03-29 19:33:18 +0100

    cover all moderation views with tests

--- server/apps/main/tests/test_moderate_views.py
+++ server/apps/main/tests/test_moderate_views.py
@@ -43,6 +43,7 @@ class ModerationViewTests(TestCase):
         # create public expierence that needs review
         pe_data = {
             'experience_text': "test",
+            'title_text': "title",
             'open_humans_member': self.non_moderator_ohmember,
             'experience_id': 'test-test-test'
         }
@@ -70,6 +71,28 @@ class ModerationViewTests(TestCase):
                              status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'main/home.html')
 
+    def test_single_moderation_view_as_guest_post(self):
+        """
+        Test moderating single experience page fails when done by non-autenticated user
+        """
+        
+        c = Client()        
+        response = c.post("/main/moderate/test-test-test/",
+                        {"mentalhealth": True, "other":"New trigger",
+                        "moderation_status":"approved",
+                        "moderation_comments":"amazing story!",
+                        },
+                        follow=True)
+        # assert ending up on right page
+        self.assertRedirects(response, "/",
+                             status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'main/home.html')
+        # test that no changes were made
+        pe = PublicExperience.objects.get(experience_id='test-test-test')
+        self.assertEqual(pe.mentalhealth,False)
+        self.assertEqual(pe.other,"")
+        self.assertEqual(pe.moderation_status,"not reviewed")
+
     # test moderation pages as logged-in but non-moderator user
 
     def test_view_moderate_list_non_moderator(self):
@@ -94,6 +117,30 @@ class ModerationViewTests(TestCase):
                              status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'main/home.html')
 
+    def test_single_moderation_view_as_non_moderator_post(self):
+        """
+        Test moderating single experience page fails when done by non-moderator
+        """
+        
+        c = Client()
+        c.force_login(self.non_moderator_user)
+        
+        response = c.post("/main/moderate/test-test-test/",
+                        {"mentalhealth": True, "other":"New trigger",
+                        "moderation_status":"approved",
+                        "moderation_comments":"amazing story!",
+                        },
+                        follow=True)
+        # assert ending up on right page
+        self.assertRedirects(response, "/main/overview",
+                             status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'main/home.html')
+        # test that no changes were made
+        pe = PublicExperience.objects.get(experience_id='test-test-test')
+        self.assertEqual(pe.mentalhealth,False)
+        self.assertEqual(pe.other,"")
+        self.assertEqual(pe.moderation_status,"not reviewed")
+
     # test moderation pages as logged-in moderator
 
     def test_view_moderate_list_moderator(self):
@@ -115,3 +162,58 @@ class ModerationViewTests(TestCase):
         response = c.get("/main/moderate/test-test-test/", follow=True)
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'main/moderate_experience.html')
+
+
+    def test_single_moderation_view_as_moderator_post_missing_data(self):
+        """
+        Test moderate single experience page is accessible by moderators but access has been revoked
+        """
+        c = Client()
+        c.force_login(self.moderator_user)
+        self.assertEqual(len(PublicExperience.objects.all()),1)
+        response = c.post("/main/moderate/test-test-test/",
+                          {"mentalhealth": True, "other":"New trigger",
+                           },
+                          follow=True)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'main/moderate_public_experiences.html')
+        # Public experience should be deleted as no longer access on OH
+        self.assertEqual(len(PublicExperience.objects.all()),0)
+
+    def test_single_moderation_view_as_moderator_post(self):
+        """
+        Test moderate single experience page is successfully accessible and editable by moderators
+        """
+        
+        c = Client()
+        c.force_login(self.moderator_user)
+        # Test uses cassette to allow fake-upload to OH
+        with vcr.use_cassette('server/apps/main/tests/fixtures/moderate_experience.yaml',
+                      filter_query_parameters=['access_token'],match_on=['path']):
+            response = c.post("/main/moderate/test-test-test/",
+                            {"mentalhealth": True, "other":"New trigger",
+                            "moderation_status":"approved",
+                            "moderation_comments":"amazing story!",
+                            },
+                            follow=True)
+        # assert ending up on right page
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'main/moderate_public_experiences.html')
+        # test that all changes were made
+        pe = PublicExperience.objects.get(experience_id='test-test-test')
+        self.assertEqual(pe.mentalhealth,True)
+        self.assertEqual(pe.other,"New trigger")
+        self.assertEqual(pe.moderation_status,"approved")
+        # Try injecting additional information that should be immutable 
+        # Should raise KeyError in form
+        with self.assertRaises(KeyError) as ce:
+            response = c.post("/main/moderate/test-test-test/",
+                            {"mentalhealth": True, "other":"New trigger",
+                            "moderation_status":"approved",
+                            "moderation_comments":"amazing story!",
+                            "title_text": 'try injection!'
+                            },
+                            follow=True)
+        # Check that key-error was caused by title text
+        self.assertEqual(str(ce.exception),"'title_text'")
+            
