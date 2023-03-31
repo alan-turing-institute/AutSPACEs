6dd3196c735f896bf95e9e2f9c41b3218d2fa7b9
Helen Duncan <hduncan@mac-ati1021.home>
2023-03-31 05:20:17 +0100

    streamline test

--- server/apps/main/tests/test_views.py
+++ server/apps/main/tests/test_views.py
@@ -3,6 +3,8 @@ from django.contrib import auth
 from django.test import Client
 from django.db import models
 from server.apps.main.models import PublicExperience
+from django.contrib.auth.models import User
+
 
 from openhumans.models import OpenHumansMember
 
@@ -16,18 +18,32 @@ class Views(TestCase):
         """
         Set-up for test with two users
         """
-        user_a = auth.get_user(self.client)
-        assert user_a.is_authenticated
+        # user_a = auth.get_user(self.client)
+        # assert user_a.is_authenticated
 
         # # Two users 
         data = {"access_token": 'foo',
         "refresh_token": 'bar',
         "expires_in": 36000}
+
+        # Create user A
         self.oh_a = OpenHumansMember.create(oh_id=12345678,data=data)
+        self.oh_a.save()
+        self.user_a = self.oh_a.user
+        self.user_a.openhumansmember = self.oh_a
+        self.user_a.set_password('password_a')
+        self.user_a.save()
+        # Create user B
         self.oh_b = OpenHumansMember.create(oh_id=87654321,data=data)
+        self.oh_b.save()
+        self.user_b = self.oh_a.user
+        self.user_b.openhumansmember = self.oh_a
+        self.user_b.set_password('password_a')
+        self.user_b.save()
+
+    
 
         
-
         # Each with a public experience
         pe_data = {"experience_text": "Here is some experience text",
                       "difference_text": "Here is some difference text",
@@ -36,42 +52,32 @@ class Views(TestCase):
         self.pe_b = PublicExperience(open_humans_member=self.oh_b, experience_id="8765", **pe_data)
 
 
-
     def test_share_exp(self):
+        """Check that no-authorised users are redirected (to index, then home) - get"""
         # c = Client()
         # logged_in = c.login(username='testuser', password='12345')
         # Check that a non-logged in user cannot share an experience
         # An unlogged in user should get redirected to the index (302 response)
-        non_logged_in_user = Client()
-        response = non_logged_in_user.get('/main/share_exp/')
+        c = Client()
+        response = c.get('/main/share_exp/')
         assert response.status_code == 302
         print(response.status_code)
 
         print("*********")
         
-        from django.contrib.auth.models import User
-        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-        user.save()
-        logged_in_user = Client()
-        logged_in_user.force_login(user)
-        response_l = logged_in_user.get('/main/share_exp/')
-        print("Logged in ", response_l.status_code)
+        # from django.contrib.auth.models import User
+        # user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        # user.save()
+        d = Client()
+        d.force_login(self.user_a)
+        response_d = d.get('/msin/share_exp/')
+        print("logged in - ", response_d.status_code)
+        # logged_in_user = Client()
+        # logged_in_user.force_login(user)
+        # response_l = logged_in_user.get('/main/share_exp/')
+        # print("Logged in ", response_l.status_code)
         # print(self.pe_a)
 
         # assert isinstance(self.pe_a, PublicExperience)
         assert 1==1
-        # data = {"access_token": "foo", "refresh_token": "bar", "expires_in": 36000}
-        # self.non_moderator_user = OpenHumansMember.create(oh_id=12345678, data=data)
-        # self.moderator_user = OpenHumansMember.create(oh_id=23456789, data=data)
-        # self.moderator_group = Group.objects.create(name="SecondGroup")
-        # self.moderator_group.user_set.add(self.moderator_user.user)
-        # self.moderator_group = Group.objects.create(name="Moderators")
-        # self.moderator_group.user_set.add(self.moderator_user.user)
 
-    # def test_reformat_date(self):
-    #      """
-    #      Test that formating OH timestamp to native time object works
-    #      """
-    #      year = int(self.context['files'][0]['created'][:4])
-    #      returned_context = reformat_date_string(self.context)
-    #      self.assertEqual(returned_context['files'][0]['created'].year,year)
