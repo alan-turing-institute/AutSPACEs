c563f1563026016c04fcdd04bb5a8e754873065d
Helen Duncan <hduncan@turing.ac.uk>
2023-06-20 16:29:31 +0100

    tidier functions - thanks DLJ

--- static/js/shared-stories-filtering.js
+++ static/js/shared-stories-filtering.js
@@ -1,21 +1,12 @@
-let abuse = document.getElementById("abuse-checkbox");
-let drug = document.getElementById("drug-checkbox");
-let negbody = document.getElementById("negbody-checkbox");
-let violence = document.getElementById("violence-checkbox");
-let mentalhealth = document.getElementById("mentalhealth-checkbox");
-let other = document.getElementById("other-checkbox");
-let all = document.getElementById("all-checkbox");
-
 let searchForm = document.getElementById("search-form");
 
-abuse.addEventListener("change", pressSubmit);
-drug.addEventListener("change", pressSubmit);
-negbody.addEventListener("change", pressSubmit);
-violence.addEventListener("change", pressSubmit);
-mentalhealth.addEventListener("change", pressSubmit);
-other.addEventListener("change", pressSubmit);
-all.addEventListener("change", isAllChecked)
-
+$(function() {
+    $(".custom-control-input").on("change", pressSubmit)
+  
+    function pressSubmit() {
+        $("#search-form").submit()
+    }
+  })
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     var scrollpos = localStorage.getItem('scrollpos');
@@ -27,13 +18,4 @@ window.onbeforeunload = function(e) {
 };
 
 
-function isAllChecked(){
-    if(all.checked){
-        pressSubmit();
-    }
-}
-
-function pressSubmit() {
-    searchForm.submit();
-}
   
