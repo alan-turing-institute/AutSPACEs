2f61f9c219b369e51a4873fa188022e533f69e63
Helen Duncan <hduncan@turing.ac.uk>
2023-06-27 15:30:44 +0100

    alert debugging

--- static/js/shared-stories-filtering.js
+++ static/js/shared-stories-filtering.js
@@ -1,12 +1,39 @@
 let searchForm = document.getElementById("search-form");
 
+// Check if the all toggle has been changed
+let all = document.getElementById("all-checkbox");
+let abuse = document.getElementById("abuse-checkbox")
+let vio = document.getElementById('violence-chcekbox')
+
+
+
 $(function() {
     $(".custom-control-input").on("change", pressSubmit)
-  
+
     function pressSubmit() {
         $("#search-form").submit()
+
+        var state_after_submit_all = $("#all-checkbox").is(":checked")
+
+        var state_after_submit_indi = [
+            $("#abuse-checkbox").is(":checked"),
+            $("#drug-checkbox").is(":checked"),
+            $("#negbody-checkbox").is(":checked"),
+            $("#violence-checkbox").is(":checked"),
+            $("#mentalhealth-checkbox").is(":checked"),
+            $("#other-checkbox").is(":checked"),
+        ]
+
+        var isAllTrue = (x) => x == true;
+
+        var all_checked = state_after_submit_indi.every(isAllTrue)
+        alert("all " + state_after_submit_all + " indi " + state_after_submit_indi + "-----" + all_checked)
     }
+
+
   })
+
+var state_after_submit = [$("#violence-checkbox").is(":checked"), $("#abuse-checkbox").is(":checked")]
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     var scrollpos = localStorage.getItem('scrollpos');
