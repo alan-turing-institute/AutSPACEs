ff00ea65d79eeb6bfbadb348fa7fc12ee89f5498
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2023-08-29 15:07:18 +0100

    link help texts for screen readers

--- server/apps/users/forms.py
+++ server/apps/users/forms.py
@@ -8,7 +8,10 @@ class UserProfileForm(forms.Form):
         ("no", "No"),
         ("unspecified", "Prefer not to say"),
     ]
-    autistic_identification = forms.ChoiceField(choices = autistic_identifications, widget = forms.Select(attrs={"class": "custom-select"}),
+    autistic_identification = forms.ChoiceField(choices = autistic_identifications, widget = forms.Select(attrs={
+        "class": "custom-select",
+        "aria-describedby": "help_id_autistic_identification"}
+        ),
                                            label="Do you identify as autistic?",
                                            help_text="AutSPACEs is focused on collecting and sharing the voices and lived experiences of autistic people, which is why this is a mandatory question. If you select <i>prefer not to say</i>, your experiences will be considered as coming from a non-autistic person, labeled as such and e.g. not used for research.",
                                            required=True,
@@ -23,7 +26,11 @@ class UserProfileForm(forms.Form):
         ("over 65", "Over 65"),
         ("unspecified", "Prefer not to say"),
     ]
-    age_bracket = forms.ChoiceField(choices = age_brackets, widget = forms.Select(attrs={"class": "custom-select"}), required=False,
+    age_bracket = forms.ChoiceField(choices = age_brackets, widget = forms.Select(attrs={
+        "class": "custom-select",
+        "aria-describedby": "help_id_age_bracket"}
+        ), 
+                                   required=False,
                                    label="What is your age group?",
                                    help_text="Sharing your age might help researchers understand potential trends across age groups. It can also help readers to better understand your perspective if this data is made public. You can also choose to self-identify instead of using one of the pre-given options.",
                                    initial="unspecified")
@@ -42,7 +49,11 @@ class UserProfileForm(forms.Form):
         ("self_identify", "Self identify"),
         ("unspecified", "Prefer not to say"),
     ]
-    gender = forms.ChoiceField(choices = genders, widget = forms.Select(attrs={"class": "custom-select"}), required=False,
+    gender = forms.ChoiceField(choices = genders, widget = forms.Select(attrs={
+        "class": "custom-select",
+        "aria-describedby": "help_id_gender"}
+        ), 
+                              required=False,
                               label="What gender do you identify with?",
                               help_text="Sharing your gender identity might help researchers understand potential trends across different demographics. It can also help readers to better understand your perspective if this data is made public.",
                               initial="unspecified")
@@ -68,7 +79,8 @@ class UserProfileForm(forms.Form):
                                  help_text="You can provide any additional context about yourself here that you deem relevant to understanding your perspective and experiences. The AutSPACEs team will potential use your information to refine the structured demographic questions above. You can also make this information publicly accessible &mdash; which will be linked to your public experiences.",
                                  widget=forms.Textarea(attrs={"placeholder":"Potential things you could talk about here include your ethnicity, whether you grew up/are living in a rural or urban environment, your educational background or really anything else that comes to your mind.",
                                                               "rows":"4",
-                                                              "class":"form-control"}))
+                                                              "class":"form-control",
+                                                              "aria-describedby": "help_id_description"}))
     description.group = 1
 
     description_public = forms.BooleanField(label = "Make description public",
