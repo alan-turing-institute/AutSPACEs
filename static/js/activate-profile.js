b5065f7a17d638df73b3133dfe25ed562c78911b
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2023-08-30 17:39:43 +0100

    add migration & review eddits

--- static/js/activate-profile.js
+++ static/js/activate-profile.js
@@ -25,7 +25,7 @@ $(function () {
   );
 });
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// JavaScript for disabling form submissions if there are invalid fields
 (function() {
   'use strict';
   window.addEventListener('load', function() {
