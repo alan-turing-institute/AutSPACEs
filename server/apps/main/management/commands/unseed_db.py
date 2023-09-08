096999b648c8d95fb917ca0eed3cb5fb5e356400
Helen Duncan <hduncan@turing.ac.uk>
2023-09-08 15:26:31 +0100

    Remove seeded user

--- server/apps/main/management/commands/unseed_db.py
+++ server/apps/main/management/commands/unseed_db.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from server.apps.main.models import PublicExperience
 from server.apps.users.models import UserProfile
+from django.contrib.auth.models import User
 from server.apps.users.helpers import delete_user
 
 class Command(BaseCommand):
@@ -9,11 +10,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # Delete user added stories from OH and DB and delete the user
-        for u in UserProfile.objects.all():
-            delete_user(user=u.user, delete_oh_data=True)
+        for up in UserProfile.objects.all():
+            delete_user(user=up.user, delete_oh_data=True)
 
         # Delete the seeded stories from the DB
         for p in PublicExperience.objects.all():
             p.delete()
+
+        # Delete seeded user
+        for u in User.objects.all():
+            if u.username == "999999999_openhumans":
+                u.delete()
+
 
 
