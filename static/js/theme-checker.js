a404de24c85360e69aec3348796195d7c00b38f7
Helen Duncan <hduncan@turing.ac.uk>
2024-02-12 00:09:10 +0000

    rm js print statements

--- static/js/theme-checker.js
+++ static/js/theme-checker.js
@@ -4,25 +4,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let storedTheme = localStorage.getItem('theme');
 
     if (storedTheme !== null) {
-        console.log("The user has set a theme before - it is " + storedTheme);
         document.body.setAttribute('data-bs-theme', storedTheme);
         footer.setAttribute('data-bs-theme', storedTheme);
-        console.log("The theme has been set to " + storedTheme);
     } else {
-        console.log("The user has not set a theme before");
         // Get the system theme
         let systemTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
         document.body.setAttribute('data-bs-theme', systemTheme);
         footer.setAttribute('data-bs-theme', systemTheme);
-        console.log("The theme has been set to the system theme: " + systemTheme);
     }
 });
 
 
 document.getElementById('theme-toggle').addEventListener('click', function() {
     let currentTheme = document.body.getAttribute('data-bs-theme');
-    console.log("Before clicking the current theme is " + currentTheme + " mode");
-    console.log('Theme toggle clicked')
     if (currentTheme === 'dark') {
         document.body.setAttribute('data-bs-theme', 'light');
         footer.setAttribute('data-bs-theme', 'light');
